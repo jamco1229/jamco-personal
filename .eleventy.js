fc2fc0e2af0c786d1a74e650b247fee0366466db
jamco1229 <121612964+jamco1229@users.noreply.github.com>
2023-02-23 10:36:52 +0000

    reljhb

--- .eleventy.js
+++ .eleventy.js
@@ -8,22 +8,25 @@ const fs = require("fs");
 
 var getIndex = (collection, currentSlug) => {
   let currentIndex = 0;
-  collection.filter((page, index) => {
-    currentIndex = page.fileSlug == currentSlug ? index : currentIndex;
+  collection.forEach((page, index) => {
+    if (page.fileSlug == currentSlug) {
+      currentIndex = index;
+      return;
+    }
   });
   return currentIndex;
 };
 
 module.exports = function (eleventyConfig) {
   // Plugins
-  config.addPlugin(pluginRss);
-  config.addPlugin(pluginNav);
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(pluginNav);
 
   // Filters
-  config.addFilter("dateToFormat", (date, format) => {
+  eleventyConfig.addFilter("dateToFormat", (date, format) => {
     return DateTime.fromJSDate(date, { zone: "utc" }).toFormat(String(format));
   });
-  config.addFilter("yearRange", (date1Str, date2Str = false) => {
+  eleventyConfig.addFilter("yearRange", (date1Str, date2Str = false) => {
     let date = false;
     let end = false;
     let start = false;
@@ -38,7 +41,7 @@ module.exports = function (eleventyConfig) {
       end = date2;
     }
     const endYear = end.toFormat("yyyy");
-    if (start && end) {
+    if (date1 && end) {
       const startYear = start.toFormat("yyyy");
       if (startYear == endYear) {
         date = endYear;
@@ -56,132 +59,5 @@ module.exports = function (eleventyConfig) {
     }
     return date;
   });
-  config.addFilter("dateToISO", (date) => {
-    return DateTime.fromJSDate(date, { zone: "utc" }).toISO({
-      includeOffset: false,
-      suppressMilliseconds: true,
-    });
-  });
-  config.addFilter("nextInCollection", (collection, currentSlug) => {
-    const currentIndex = getIndex(collection, currentSlug);
-    const pages = collection.filter((page, index) => {
-      return index == currentIndex + 1 ? page : false;
-    });
-    return pages.length ? pages[0] : false;
-  });
-  config.addFilter("prevInCollection", (collection, currentSlug) => {
-    const currentIndex = getIndex(collection, currentSlug);
-    const pages = collection.filter((page, index) => {
-      return index == currentIndex - 1 ? page : false;
-    });
-    return pages.length ? pages[0] : false;
-  });
-  config.addFilter("getPagesByPaths", (collection, paths) => {
-    let pages = [];
-    paths.forEach((path) => {
-      let page = collection.filter((page) => {
-        return page.filePathStem.includes(path) ? page : false;
-      });
-      if (page.length) {
-        pages.push(page[0]);
-      }
-    });
-    return pages;
-  });
-  config.addFilter("getFeaturedImage", (blocks) => {
-    // Get the featured images
-    let images = blocks.filter((block) => {
-      return block.type == "image" && block.featured ? block : false;
-    });
-    // If no featured images, get all the images
-    if (!images.length) {
-      images = blocks.filter((block) => {
-        return block.type == "image" ? block : false;
-      });
-    }
-    return images.length ? images[0] : false;
-  });
-
-  // Collections
-  eleventyConfig.addCollection("projects", function (collection) {
-    return collection.getFilteredByGlob("./content/projects/*.md");
-  });
-  config.addCollection("projects", (collection) => {
-    const projects = collection.getFilteredByGlob("content/projects/*.md");
-    return projects.sort(function (a, b) {
-      return b.data.dateEnd - a.data.dateEnd;
-    }); 
-  }); 
-  config.addCollection("posts", function (collection) {
-    const posts = collection.getFilteredByGlob("content/posts/*.md");
-    return posts.sort(function (a, b) {
-      return b.data.date - a.data.date;
-    });
-  });
-  config.addCollection("pages", function (collection) {
-    return collection.getFilteredByGlob("content/pages/*.md");
-  });
-
-  // Markdown
-  const markdownOptions = {
-    html: true,
-    breaks: true,
-    linkify: true,
-    typographer: true,
-  };
-  const markdownLibrary = markdownIt(markdownOptions).use(markdownItAnchor, {
-    permalink: false,
-  });
-  config.setLibrary("md", markdownLibrary);
-  config.addNunjucksFilter("markdownify", (markdownString) =>
-    markdownIt(markdownOptions).render(markdownString)
-  );
-  config.addNunjucksFilter("markdownifyInline", (markdownString) =>
-    markdownIt(markdownOptions).renderInline(markdownString)
-  );
-  config.setFrontMatterParsingOptions({
-    excerpt: true,
-    excerpt_separator: "<!--more-->", // Matches WordPress style
-  });
-
-  // BrowserSync
-  config.setBrowserSyncConfig({
-    callbacks: {
-      ready: function (err, browserSync) {
-        const content_404 = fs.readFileSync("_site/404.html");
-
-        browserSync.addMiddleware("*", (req, res) => {
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
-      },
-    },
-  });
-
-  // Pass-thru files
-  config.addPassthroughCopy({ "content/media": "media" });
-  config.addPassthroughCopy("css");
-  config.addPassthroughCopy("fonts");
-
-  // Layouts
-  config.addLayoutAlias("base", "layouts/base.njk");
-  config.addLayoutAlias("page", "layouts/page.njk");
-  config.addLayoutAlias("error", "layouts/error.njk");
-  config.addLayoutAlias("feed", "layouts/feed.njk");
-  config.addLayoutAlias("home", "layouts/home.njk");
-  config.addLayoutAlias("post", "layouts/post.njk");
-  config.addLayoutAlias("posts", "layouts/posts.njk");
-  config.addLayoutAlias("project", "layouts/project.njk");
-
-  // Base Config
-  return {
-    dir: {
-      data: "content/_data",
-    },
-    templateFormats: ["njk", "md", "html", "liquid"],
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
-    passthroughFileCopy: true,
-  };
-};
+  eleventyConfig.addFilter("dateToISO", (date) => {
+    return DateTime.fromJSDate(date, { zone: "utc" }).
