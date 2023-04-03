6acea431f86546160d1a4fc8f8d64dba6ec72985
jamco1229 <121612964+jamco1229@users.noreply.github.com>
2023-04-03 22:05:43 +0100

    gg

--- .eleventy.js
+++ .eleventy.js
@@ -135,7 +135,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addFilter("date", (dateObj, format) => {
       return LuxonDateTime.fromJSDate(dateObj).toFormat(format);
     });
-  };
+
 
   eleventyConfig.addCollection("projects", (collection) => {
     const projects = collection.getFilteredByGlob("content/projects/*.md");
