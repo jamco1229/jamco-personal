f08a5c8ba107fc50d97eb4b60bbadb355fa02683
jamco1229 <121612964+jamco1229@users.noreply.github.com>
2023-04-06 15:54:09 +0000

    fjfj

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,7 @@ const Image = require('@11ty/eleventy-img');
 async function imageShortcode(src, alt) {
   let metadata = await Image(src, {
     formats: ['webp'],
-    urlPath: '/content/media/',
+    urlPath: '/media/',
     outputDir: '_site/content/media/',
   });
 
