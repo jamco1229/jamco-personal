e6a706bdacddc7082a9a5c956987f086fa25297f
jamco1229 <121612964+jamco1229@users.noreply.github.com>
2023-04-21 11:42:25 +0000

    jgjgj

--- main.js
+++ main.js
@@ -2,30 +2,29 @@
 import tippy from 'https://cdn.skypack.dev/tippy.js';
 import 'https://cdn.skypack.dev/tippy.js/dist/tippy.css';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const contactButton = document.getElementById('contact');
-  if (contactButton) {
-    const tip = tippy(contactButton, {
-      content: 'Copy my email to your clipboard!',
-      animation: 'shift-toward-subtle',
-      arrow: 'round',
-    });
+const contactButton = document.getElementById('contact');
+if (contactButton) {
+  const tip = tippy(contactButton, {
+    content: 'Copy email to clipboard!',
+    animation: 'shift-toward-subtle',
+    arrow: 'round',
+  });
 
-    contactButton.addEventListener('click', (event) => {
-      event.preventDefault();
-      const email = 'james.coy.design@gmail.com';
+  contactButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const email = 'james.coy.design@gmail.com';
 
-      navigator.clipboard.writeText(email).then(() => {
-        tip.setContent('Email copied to clipboard!');
-        setTimeout(() => {
-          tip.setContent('Copy my email to your clipboard!');
-        }, 2000);
-      }).catch(() => {
-        console.error('Failed to copy email to clipboard.');
-      });
+    navigator.clipboard.writeText(email).then(() => {
+      tip.setContent('Copied to clipboard!');
+      setTimeout(() => {
+        tip.setContent('Copy email to clipboard!');
+      }, 2000);
+    }).catch(() => {
+      console.error('Failed to copy email to clipboard.');
     });
-  }
-});
+  });
+}
+
 
 // Code for active class in nav bar
 const navLinks = document.querySelectorAll(".site-nav a");
