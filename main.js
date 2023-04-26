6346459de60fb3c6be2e1b41db5bba44501d89f6
jamco1229 <121612964+jamco1229@users.noreply.github.com>
2023-04-26 09:31:23 +0100

    sfgf

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-import tippy from 'tippy.js';
-import 'tippy.js/dist/tippy.css'; // optional for styling
 
 const contactButton = document.getElementById('contact');
 if (contactButton) {
@@ -24,44 +22,3 @@ if (contactButton) {
   });
 }
 
-
-// Code for active class in nav bar
-const navLinks = document.querySelectorAll(".site-nav a");
-const currentUrl = window.location.href;
-
-navLinks.forEach(function (link) {
-  if (link.href === currentUrl) {
-    link.classList.add("active");
-  } else {
-    link.classList.remove("active");
-  }
-});
-
-// Code for BlazeSlider
-const sliderEl = document.querySelector(".blaze-slider");
-const blazeSlider = new BlazeSlider(sliderEl, {
-  all: {
-    enableAutoplay: false,
-    slidesToScroll: 1,
-    slidesToShow: 1.4,
-    transitionDuration: 150,
-    loop: true,
-  },
-});
-
-blazeSlider.onSlide((pageIndex, firstVisibleSlideIndex, lastVisibleSlideIndex) => {
-  console.log({ pageIndex, firstVisibleSlideIndex, lastVisibleSlideIndex });
-});
-
-
-// Code for smooth scroll
-const tocLinks = document.querySelectorAll(".toc a");
-
-tocLinks.forEach((link) => {
-  link.addEventListener("click", function (event) {
-    event.preventDefault();
-    const targetId = this.getAttribute("href");
-    const targetElement = document.querySelector(targetId);
-    targetElement.scrollIntoView({ behavior: "smooth" });
-  });
-});
